473edc33005fe61e0bc8e6225f5c5a1bfa0832e1
Diogo Mendes <diogoafm6@gmail.com>
2022-02-03 14:39:31 +0000

    error handler

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import './SignUp.css';
 import {useForm} from "react-hook-form";
 import { login } from "../firebase";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { Alert } from 'react-bootstrap';
+import validator from 'validator'
 
 const LoginPage = () => {
     const {register, handleSubmit} = useForm()
     let history = useHistory()
+    const [error, setError] = useState("");
+    const [gotosignup, setGotosignup] = useState(false)
 
     const redirect = () => {
         history.push("/dashboard")
@@ -14,11 +18,30 @@ const LoginPage = () => {
 
 
     const onSubmit = async (data, e) => {
-        
-        await login(data.email, data.password)
-        e.target.reset(data)
-        redirect()
-        
+
+        if(!(validator.isEmail(data.email))){
+            setError('Thats not a email')
+            e.target.reset(data)
+            return
+        }
+
+        try{
+            await login(data.email, data.password)
+            e.target.reset(data)
+            redirect()
+        }catch(error){
+            if(error.message === 'Firebase: Error (auth/user-not-found).'){
+                setError('Email not found, go to signup')
+                setGotosignup(true)
+                e.target.reset(data)
+                return
+            }
+            if(error.message === 'Firebase: Error (auth/wrong-password).'){
+                setError('Password is wrong')
+                e.target.reset(data)
+                return
+            }
+        }
     }
 
     return (
@@ -27,6 +50,8 @@ const LoginPage = () => {
                 <form onSubmit={handleSubmit(onSubmit)} id="signUpForm">
                     <fieldset>
                         <h1> Login </h1>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        {gotosignup && <div id="linkSignup" className="mb-3"><Link to="/signup"> Signup </Link></div>}
                         <input placeholder="email" {...register("email", { required: true, maxLength: 50})} />
                         <input type="password" placeholder="password" {...register("password", {required: true, maxLength:20})}/>
                         <input type="submit" value="Submit"/>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SignUp.css'
 import {useForm} from "react-hook-form";
 import { signup } from '../firebase';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { Alert } from 'react-bootstrap';
+import validator from 'validator'
 
 const SignUp = () => {
     const {register, handleSubmit} = useForm();
+    const [error, setError] = useState("");
+    const [gotologin, setGotologin] = useState(false)
     let history = useHistory()
 
     const redirect = () => {
@@ -16,24 +20,36 @@ const SignUp = () => {
         const email = data.email
         const password = data.password
         const repeatpassword = data.repeatpassword
-        
-        if(!(password && repeatpassword)){
-            return 
+
+
+        if(!(validator.isEmail(email))){
+            setError('Thats not a email')
+            e.target.reset(data)
+            return
         }
-        
-        if(!(password === repeatpassword)) {
-            console.log('palavras são diferents')
+
+        if(!(password && repeatpassword)){
+            setError('The passwords are diferent')
+            e.target.reset(data)
             return
         }
 
         if((password.length < 7)){
-            console.log('word é pequena')
+            setError('Your password is small')
+            e.target.reset(data)
             return
         }
 
-        await signup( email, password)
-        e.target.reset(data)
-        redirect()
+        try{
+            await signup( email, password)
+            e.target.reset(data)
+            redirect()
+        }catch(err) {
+            setError("This Email is already register")
+            setGotologin(true)
+            e.target.reset(data)
+            return
+        }
 
     }
 
@@ -43,6 +59,8 @@ const SignUp = () => {
                 <form onSubmit={handleSubmit(onSubmit)} id="signUpForm">
                     <fieldset>
                         <h1> Sign up </h1>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        {gotologin && <div id="linkSignup" className="mb-3"><Link to="/loginpage"> Login </Link></div>}
                         <input placeholder="email" {...register("email", { required: true, maxLength: 50})} />
                         <input type="password" placeholder="password" {...register("password", {required: true, maxLength:20})}/>
                         <input type="password" placeholder="repeat password" {...register("repeatpassword", {required: true, maxLength:20})}/>
