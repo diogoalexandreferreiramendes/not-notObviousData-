3020222009e6a3c97a0606dade24cd4266e78e9f
Diogo Mendes <diogoafm6@gmail.com>
2023-07-17 11:40:24 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,27 +5,6 @@ import { useEffect, useState } from "react";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDLG5Zi19hwYWMM659LIMn6stXGglvKokA",
-  authDomain: "nod-production.firebaseapp.com",
-  projectId: "nod-production",
-  storageBucket: "nod-production.appspot.com",
-  messagingSenderId: "109875278953",
-  appId: "1:109875278953:web:bafc75a38a755e6023def8"
-};
-
-//This one is for development
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC2sIpoy83W-txwAieIJWQpCKQ4eY5npo8",
-//     authDomain: "nod-development.firebaseapp.com",
-//     projectId: "nod-development",
-//     storageBucket: "nod-development.appspot.com",
-//     messagingSenderId: "857912163401",
-//     appId: "1:857912163401:web:4c5564f8d2977be70ad398"
-//   };
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth()
